1748fe4b63979f877f82b42738b717bfb203f58b
main <mokomokohituji@users.noreply.github.com>
2021-08-15 21:18:13 +0900

    処理をメソッド化した

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,6 +17,39 @@ public class Main {
 
         //ユーザーに入力した手を表示させるためpyへ代入
         //空文字をいれなくとも今回は宣言だけすればいいので省略、cpも同じ変更を行う
+        String py = playComparison(play);
+
+        System.out.println("あなたの手は" + py + "です");
+
+
+        //コンピュータの手を決めるためランダム関数を使う
+        Random rand = new Random();
+        //0から2の範囲まで乱数が代入される
+        int comp = rand.nextInt(3);
+
+        //ユーザーにコンピューターの手を表示させるためcpへ代入
+        String cp = compComparison(comp);
+
+        System.out.println("コンピュータの手は" + cp + "です");
+
+        //プレイヤーとコンピュータの手を比較する,メソッド化した
+        comparison(play,comp);
+
+        //Scannerは必ず閉じる必要がある。変数.close();変数からcloseメソッドを呼び出し閉じる。
+        scan.close();
+    }
+
+    public static void comparison(int play,int comp){
+        if(play == comp) { //同じ手なら
+            System.out.println("あいこです");
+        }else if(play == 0 && comp == 2|| play == 1 && comp == 0|| play == 2 && comp == 1) {
+            System.out.println("あなたの負けです");
+        }else  {
+            System.out.println("あなたの勝ちです");
+        }
+    }
+
+    public static String playComparison(int play){
         String py;
         switch (play) {
             case 0:
@@ -29,16 +62,10 @@ public class Main {
                 py = "パー";
                 break;
         }
+        return py;
+    }
 
-        System.out.println("あなたの手は" + py + "です");
-
-
-        //コンピュータの手を決めるためランダム関数を使う
-        Random rand = new Random();
-        //0から2の範囲まで乱数が代入される
-        int comp = rand.nextInt(3);
-
-        //ユーザーにコンピューターの手を表示させるためcpへ代入
+    public static String compComparison(int comp){
         String cp;
         switch (comp) {
             case 0:
@@ -52,20 +79,7 @@ public class Main {
                 cp = "パー";
                 break;
         }
-
-        System.out.println("コンピュータの手は" + cp + "です");
-
-        //プレイヤーとコンピュータの手を比較する
-        if(play == comp) { //同じ手なら
-            System.out.println("あいこです");
-        }else if(play == 0 && comp == 2|| play == 1 && comp == 0|| play == 2 && comp == 1) {
-            System.out.println("あなたの負けです");
-        }else  {
-            System.out.println("あなたの勝ちです");
-        }
-
-        //Scannerは必ず閉じる必要がある。変数.close();変数からcloseメソッドを呼び出し閉じる。
-        scan.close();
+        return cp;
     }
 }
 
